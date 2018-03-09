e54201eee972586d96e8657e42d41171aca9e671
Jorge Rodrigues <jorgemfvrodrigues@gmail.com>
2018-03-09 11:33:28 +0000

    Fix setInterval problem

--- fixed_footer/assets/js/footer-fixed.js
+++ fixed_footer/assets/js/footer-fixed.js
@@ -1,22 +1,3 @@
-/* =============================================================
- * bootstrap-typeahead.js v2.3.2
- * http://twbs.github.com/bootstrap/javascript.html#typeahead
- * =============================================================
- * Copyright 2013 Twitter, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ============================================================ */
-
 function fixFooter() {
   var docHeight = $(window).height();
   var footerHeight = $('#footer').height();
@@ -28,4 +9,4 @@ function fixFooter() {
 }
 
 //
-setInterval(function(){fixFooter();}, 0);
+setInterval(function(){fixFooter()}, 0);
